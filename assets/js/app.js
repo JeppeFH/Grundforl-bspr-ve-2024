de2ccf40654c9bc5ebbf1713ea2884149b57c2f9
JeppeFH <jeppefinch@hotmail.dk>
2024-06-21 11:21:41 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-/*Navigation*/
+//NAVIGATION//
 let burgerMenuOpen = document.querySelector(".burgerMenuOpen");
 let burgerMenuClose = document.querySelector(".burgerMenuClose");
 let navLinks = document.querySelector(".nav-links");
@@ -23,3 +23,74 @@ burgerMenuClose.addEventListener("click", () => {
   /*Laver et click event på burgermenuclose som remover active class ved 
   click. */
 });
+
+//FORM VALIDERING//
+let form = document.querySelector(".form");
+
+form.addEventListener("submit", function (event) {
+  /*tilføjer event listener til form. Lytter specifikt efter "submit" event, 
+  hvilket sker, når brugeren forsøger at sende formularen enten ved at trykke 
+  på en submit-knap*/
+  event.preventDefault;
+  /*Når brugeren prøver at sende formularen, aktiveres vores 
+  event listener-funktion, som straks kalder event.preventDefault(). 
+  Dette forhindrer, at formularen bliver sendt og siden genindlæses, 
+  hvilket giver os mulighed for at håndtere formularens data på vores 
+  egen måde. For eksempel kan vi tjekke om brugeren har udfyldt alle 
+  felterne korrekt eller sende dataene til serveren uden at genindlæse 
+  siden.*/
+});
+
+//variabeldeklarationer
+let nameInput = document.querySelector("#nameInput");
+let mailInput = document.querySelector("#mailIput");
+let textArea = document.querySelector("#textArea");
+let submitInput = document.querySelector("#submitIput");
+
+// Initialiser variabler
+let isValid = true;
+/* boolean variabel; en simpel datatype, der kun kan have én af to værdier: 
+true eller false. */
+let errorMessage = [];
+
+// Valider navn
+if (nameInput.value.trim() === "") {
+  /*For hvert inputfelt, tjekkes om værdien (efter at have fjernet 
+  eventuelle whitespace med trim()) er tom ("").*/
+  /*=== sikrer, at sammenligningen kun returnerer true, 
+  hvis begge værdier er af samme type og har samme værdi.*/
+  isValid = false;
+  nameInput.classList.add("Error");
+  errorMessage.push("Skriv dit navn");
+} else {
+  nameInput.classList.remove("Error");
+}
+
+// Valider E-mail
+if (mailInput.value.trim() === "") {
+  /*value henter eller indstiller værdi i inputfelt:*/
+  isValid = false;
+  mailInput.classList.add("Error");
+  errorMessage.push("Skriv din E-mail");
+} else {
+  mailInput.classList.remove("Error");
+}
+
+// Valider Textarea
+if (textArea.value.trim() === "") {
+  isValid = false;
+  textArea.classList.add("Error");
+  errorMessage.push("Skriv noget tekst");
+} else {
+  textArea.classList.remove("Error");
+}
+
+/*if statement som siger at hvis alle felter er udfyldt korrekt,
+send formularen*/
+if (isValid) {
+  form.submit();
+}
+
+function validate() {
+  return true;
+}
